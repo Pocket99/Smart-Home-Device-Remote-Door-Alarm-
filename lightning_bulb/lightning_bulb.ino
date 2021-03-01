dbe13762645dace213a2196d560f2b2b11b47b19
ZIRUI QIU <48567690+Pocket99@users.noreply.github.com>
2021-03-01 10:07:05 +0800

    Add files via upload

---
+++ lightning_bulb/lightning_bulb.ino
@@ -0,0 +1,63 @@
+
+#define Trig 2 //引脚Tring 连接 IO D2
+#define Echo 3 //引脚Echo 连接 IO D3 
+
+float cm; //距离变量
+float temp; //
+const int LEDpin = 4;
+const int Red = 11;
+const int Green = 10;
+const int Blue = 9;
+char data;
+void setup() {
+  Serial.begin(9600);
+  pinMode(Trig, OUTPUT);
+  pinMode(Echo, INPUT);
+  pinMode(LEDpin, OUTPUT);
+  pinMode(Red, OUTPUT);
+  pinMode(Green, OUTPUT);
+  pinMode(Blue, OUTPUT);
+  digitalWrite(LEDpin, LOW);
+}
+ 
+void loop() {
+  digitalWrite(Trig, LOW); //给Trig发送一个低电平
+  delayMicroseconds(2);    //等待 2微妙
+  digitalWrite(Trig,HIGH); //给Trig发送一个高电平
+  delayMicroseconds(10);    //等待 10微妙
+  digitalWrite(Trig, LOW); //给Trig发送一个低电平
+  
+  temp = float(pulseIn(Echo, HIGH)); //存储回波等待时间,
+  //pulseIn函数会等待引脚变为HIGH,开始计算时间,再等待变为LOW并停止计时
+  //返回脉冲的长度
+  
+  //声速是:340m/1s 换算成 34000cm / 1000000μs => 34 / 1000
+  //因为发送到接收,实际是相同距离走了2回,所以要除以2
+  //距离(厘米)  =  (回波时间 * (34 / 1000)) / 2
+  //简化后的计算公式为 (回波时间 * 17)/ 1000
+  
+  cm = (temp * 17 )/1000; //把回波时间换算成cm
+
+
+if(cm<10){
+  analogWrite(Red,255);
+  analogWrite(Green,255);
+  analogWrite(Blue,255);
+  delay(1000);
+  }else{
+   digitalWrite(LEDpin, HIGH);
+  delay(500);
+  digitalWrite(LEDpin, LOW);
+  delay(500);
+  }
+
+
+
+  
+  Serial.print("Echo =");
+  Serial.print(temp);//串口输出等待时间的原始数据
+  Serial.print(" | | Distance = ");
+  Serial.print(cm);//串口输出距离换算成cm的结果
+  Serial.println("cm");
+  delay(100);
+}
